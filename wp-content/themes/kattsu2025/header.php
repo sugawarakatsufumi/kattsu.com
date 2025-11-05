ed3fe011a789c2e1040892e9b8ba770f211cc68d
sugawara.katsufumi <sugawarakatsufumi@sugawarakatsufumi-no-MacBook-Air-3.local>
2025-11-05 09:43:45 +0900

    フォームテスト+GTMアクセス制限

--- wp-content/themes/kattsu2025/header.php
+++ wp-content/themes/kattsu2025/header.php
@@ -2,13 +2,15 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="utf-8">
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-MDM5JMV7');</script>
-  <!-- End Google Tag Manager -->
+  <?php if ( ! is_user_logged_in() ) : ?>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MDM5JMV7');</script>
+    <!-- End Google Tag Manager -->
+  <?php endif; ?>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
   <meta name="format-detection" content="telephone=no">
@@ -19,10 +21,12 @@
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/styles.css">
 </head>
 <body <?php body_class(); ?> >
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDM5JMV7"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+<?php if ( ! is_user_logged_in() ) : ?>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDM5JMV7"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+<?php endif; ?>
 <header class="header">
   <div  class="header-inner">
     <div class="header-logo">
